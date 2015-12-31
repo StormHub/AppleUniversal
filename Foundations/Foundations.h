e7b3ba01d49bf2bc2820bc187e3815e9bed5a95b
Johnny Chow <Johnny@192-168-1-7.tpgi.com.au>
2016-01-01 00:22:27 +1000

    Initial Commit

---
+++ Foundations/Foundations.h
@@ -0,0 +1,19 @@
+//
+//  Foundations.h
+//  Foundations
+//
+//  Created by Johnny Chow on 1/01/2016.
+//  Copyright © 2016 Foundations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Foundations.
+FOUNDATION_EXPORT double FoundationsVersionNumber;
+
+//! Project version string for Foundations.
+FOUNDATION_EXPORT const unsigned char FoundationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foundations/PublicHeader.h>
+
+
