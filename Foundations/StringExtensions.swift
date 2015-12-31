b517c0811570b149572bff3c8148804be278ea8f
Johnny Chow <Johnny@192-168-1-7.tpgi.com.au>
2016-01-01 00:42:17 +1000

    Added string extension file.

---
+++ Foundations/StringExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  StringExtensions.swift
+//  Foundations
+//
+//  Created by Johnny Chow on 1/01/2016.
+//  Copyright © 2016 Foundations. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+}
